549cfcaa79931856506557b6855b2c987dfe7dff
Krillleeee <102476823+Krillleeee@users.noreply.github.com>
2024-08-12 10:55:23 +0300

    Update paw3395.c

--- src/paw3395.c
+++ src/paw3395.c
@@ -913,8 +913,9 @@ static int paw3395_async_init_configure(const struct device *dev)
 
 // checked and keep
 static void paw3395_async_init(struct k_work *work)
-{
-	struct pixart_data *data = CONTAINER_OF(work, struct pixart_data, init_work);
+{	
+	struct k_work_delayable *work2 = (struct k_work_delayable *)work;
+	struct pixart_data *data = CONTAINER_OF(work2, struct pixart_data, init_work);
 	
 	const struct device *dev = data->dev;
 
